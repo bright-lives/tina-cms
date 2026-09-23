53d27fa1fa05d0dedcc5d592c3f54b3c9d276d91
Remi <remi.vledder@childsupportghana.com>
2026-09-23 09:17:59 +0200

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tina from '@tinacms/astro/integration';
 import { tinaAdminDevRedirect } from '@tinacms/astro/vite';
 import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
   adapter: node({ mode: 'standalone' }),
   integrations: [tina()],
   vite: {
-    plugins: [tinaAdminDevRedirect()],
+    plugins: [tinaAdminDevRedirect(), tailwindcss()],
   },
 });
